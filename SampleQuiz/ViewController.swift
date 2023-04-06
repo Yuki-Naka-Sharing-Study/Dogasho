421f5aea186e163738eb6bae71c2cff8df5c1703
Yuki Naka <48172941+Yuki-Naka-Sharing-Study@users.noreply.github.com>
2023-04-06 21:14:51 +0900

    Initial Commit

---
+++ SampleQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleQuiz
+//
+//  Created by 仲優樹 on 2023/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
